2f3827d521faa53acfc91aeba77b5fe938949aaa
Marek Dajnowski <marek@dajnowski.net>
2018-12-29 20:02:51 +0100

    Messing around with firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import firebase_config from './config/firebase.js'
 
+Vue.use(VueFire)
+firebase.initializeApp(firebase_config);
+const db = firebase.firestore()
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export default db;
